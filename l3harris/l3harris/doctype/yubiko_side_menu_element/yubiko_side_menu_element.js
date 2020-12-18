458441554d785cb3199afeafea1a9f471d9b62f4
triptish <tripti.sharma8@gmail.com>
2020-12-18 18:49:11 +0000

    login_page

---
+++ l3harris/l3harris/doctype/yubiko_side_menu_element/yubiko_side_menu_element.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2020, Rahi Systems Pvt Ltd and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Yubiko Side Menu Element', {
+	// refresh: function(frm) {
+
+	// }
+});
